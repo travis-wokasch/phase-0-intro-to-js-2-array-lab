d119406417b71531af49bfa3a43f25a1b437cff3
Travis <tjwokasch@gmail.com>
2021-07-13 15:42:40 -0600

    commit tree c44540c2e5b87def0f0e607b667287def0de0edd

--- index.js
+++ index.js
@@ -1,41 +1,39 @@
-const cats = ["Milo", "Otis", "Garfield"];
+const cats = ['Milo', 'Otis', 'Garfield']
 
-    function destructivelyAppendCat() {
-        cats.push("Ralph");
-        return destructivelyAppendCat;
-    }
+function destructivelyAppendCat(){
+    cats.push('Ralph')
+}
 
-    function destructivelyPrependCat() {
-        cats.unshift("Bob");
-    }
+function destructivelyPrependCat(){
+    cats.unshift('Bob')
+}
 
-    function destructivelyRemoveLastCat() {
-        cats.pop();
-    }
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
 
-    function destructivelyRemoveFirstCat() {
-        cats.shift();
-    }
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
 
-        
-    function appendCat(name) {
-        const newCats = [...cats, "Broom"];
-        return newCats;
-        }
-        
-    function prependCat(name) {
-            const newestCats = ["Arnold", ...cats];
-            return newestCats;
-            }
-    
-    function removeLastCat(name) {
-            const newerCats = [...cats];
-            newerCats.pop()
-            return newerCats;
-                }
-    function removeFirstCat(name) {
-            const newsCats = [...cats];
-            newsCats.shift()
-            return newsCats;
-                        }
+function appendCat(name){
+    const newCats = [...cats, 'Broom']
+    return newCats
+}
 
+function prependCat(name){
+    const newCats = ['Arnold', ...cats]
+    return newCats
+}
+
+function removeLastCat(){
+    const newerCats = [...cats]
+    newerCats.pop()
+    return newerCats
+}
+
+function removeFirstCat(){
+    const newerCats = [...cats]
+    newerCats.shift()
+    return newerCats
+}
